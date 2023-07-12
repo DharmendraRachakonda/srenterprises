15f81960d6b719315f1f9f6fab201deae2e3f420
DharmendraRachakonda <70020326+DharmendraRachakonda@users.noreply.github.com>
2023-07-12 23:13:26 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,101 +1,96 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Your code here
-const one = document.querySelector(".one");
-const two = document.querySelector(".two");
-const three = document.querySelector(".three");
-const four = document.querySelector(".four");
-const five = document.querySelector(".five");
-const Papa = require('./lib/papaparse.min.js');
+    const one = document.querySelector(".one");
+    const two = document.querySelector(".two");
+    const three = document.querySelector(".three");
+    const four = document.querySelector(".four");
+    const five = document.querySelector(".five");
 
-// Load CSV data on window load
-window.addEventListener("load", function() {
-    console.log("Window loaded");
+    // Load CSV data on window load
+    window.addEventListener("load", function() {
+        console.log("Window loaded");
 
-    Papa.parse("orders.csv", {
-        header: true,
-        download: true,
-        complete: function(results) {
-            console.log("CSV data parsed");
-            const data = results.data;
-            const urlParams = new URLSearchParams(window.location.search);
-            const orderId = urlParams.get("orderId");
-            console.log("Order ID from URL:", orderId);
+        Papa.parse("orders.csv", {
+            header: true,
+            download: true,
+            complete: function(results) {
+                console.log("CSV data parsed");
+                const data = results.data;
+                const urlParams = new URLSearchParams(window.location.search);
+                const orderId = urlParams.get("orderId");
+                console.log("Order ID from URL:", orderId);
 
-            // Find the matching order in the CSV data
-            const order = data.find(item => item.OrderID === orderId);
-            if (order) {
-                const orderStatus = order.OrderStatus;
-                console.log("Order status from CSV:", orderStatus);
-                const statusIndex = getStatusIndex(orderStatus);
-                if (statusIndex > 0) {
-                    // Update progress bar based on the status index
-                    updateProgressBar(statusIndex);
-                    console.log("Progress bar updated");
+                // Find the matching order in the CSV data
+                const order = data.find(item => item.OrderID === orderId);
+                if (order) {
+                    const orderStatus = order.OrderStatus;
+                    console.log("Order status from CSV:", orderStatus);
+                    const statusIndex = getStatusIndex(orderStatus);
+                    if (statusIndex > 0) {
+                        // Update progress bar based on the status index
+                        updateProgressBar(statusIndex);
+                        console.log("Progress bar updated");
+                    }
                 }
             }
-        }
+        });
     });
-});
 
-// Helper function to get the index of the status
-function getStatusIndex(status) {
-    switch (status) {
-        case "Order Confirmed":
-            return 1;
-        case "Enroute to Load Zone":
-            return 2;
-        case "Material Loaded":
-            return 3;
-        case "Enroute to Offload Zone":
-            return 4;
-        case "Order Delivered":
-            return 5;
-        default:
-            return 0;
+    // Helper function to get the index of the status
+    function getStatusIndex(status) {
+        switch (status) {
+            case "Order Confirmed":
+                return 1;
+            case "Enroute to Load Zone":
+                return 2;
+            case "Material Loaded":
+                return 3;
+            case "Enroute to Offload Zone":
+                return 4;
+            case "Order Delivered":
+                return 5;
+            default:
+                return 0;
+        }
     }
-}
 
-// Helper function to update the progress bar based on the selected index
-function updateProgressBar(index) {
-    // Remove 'active' class from all progress elements
-    one.classList.remove("active");
-    two.classList.remove("active");
-    three.classList.remove("active");
-    four.classList.remove("active");
-    five.classList.remove("active");
+    // Helper function to update the progress bar based on the selected index
+    function updateProgressBar(index) {
+        // Remove 'active' class from all progress elements
+        one.classList.remove("active");
+        two.classList.remove("active");
+        three.classList.remove("active");
+        four.classList.remove("active");
+        five.classList.remove("active");
 
-    // Add 'active' class to the selected progress element
-    switch (index) {
-        case 1:
-            one.classList.add("active");
-            break;
-        case 2:
-            one.classList.add("active");
-            two.classList.add("active");
-            break;
-        case 3:
-            one.classList.add("active");
-            two.classList.add("active");
-            three.classList.add("active");
-            break;
-        case 4:
-            one.classList.add("active");
-            two.classList.add("active");
-            three.classList.add("active");
-            four.classList.add("active");
-            break;
-        case 5:
-            one.classList.add("active");
-            two.classList.add("active");
-            three.classList.add("active");
-            four.classList.add("active");
-            five.classList.add("active");
-            break;
-        default:
-            break;
+        // Add 'active' class to the selected progress element
+        switch (index) {
+            case 1:
+                one.classList.add("active");
+                break;
+            case 2:
+                one.classList.add("active");
+                two.classList.add("active");
+                break;
+            case 3:
+                one.classList.add("active");
+                two.classList.add("active");
+                three.classList.add("active");
+                break;
+            case 4:
+                one.classList.add("active");
+                two.classList.add("active");
+                three.classList.add("active");
+                four.classList.add("active");
+                break;
+            case 5:
+                one.classList.add("active");
+                two.classList.add("active");
+                three.classList.add("active");
+                four.classList.add("active");
+                five.classList.add("active");
+                break;
+            default:
+                break;
+        }
     }
-}
-
-
 });
-
